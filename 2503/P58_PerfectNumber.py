55b9c03e2f53c10cbca4ea45db51b3e8f775dca4
Sindhu17020103 <sindhuja.golagani@techolution.com>
2026-03-25 19:24:57 +0530

    Added changes

--- 2503/P58_PerfectNumber.py
+++ 2503/P58_PerfectNumber.py
@@ -11,9 +11,9 @@
 
 def perfect_number(number):
     sum = 0
-    for x in range(1, number):
-        if number % x == 0:
-            sum += x
+    for divisor in range(1, number):
+        if number % divisor == 0:
+            sum += divisor
     return sum == number
 
 if __name__ == '__main__':
