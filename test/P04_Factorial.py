0cbf7009876f5ea42f1140fdb55baa2f3326e279
Sindhu17020103 <sindhuja.golagani@techolution.com>
2026-04-03 17:27:07 +0530

    Added python changes

--- test/P04_Factorial.py
+++ test/P04_Factorial.py
@@ -5,7 +5,9 @@
 def factorial(number):
     '''This function finds the factorial of the number passed as argument'''
     if number < 0:
+
         print('Invalid entry! Cannot find factorial of a negative number')
+        return None
     if number == 0 or number == 1:
         return 1
     else:
